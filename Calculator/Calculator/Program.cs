420633ff7deb3279b3a6641575f4f822d628c145
Kate Kalashnikova <kkalashnikova@kruxanalytics.com>
2021-06-11 15:19:36 -0600

    converted to double

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -7,8 +7,12 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Write("Enter first number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-;            
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+                        
             Console.ReadLine();
         }
     }
