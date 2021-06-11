6498a86b97d1a0f7775088985c426068f72439a6
Kate Kalashnikova <kkalashnikova@kruxanalytics.com>
2021-06-11 15:15:29 -0600

    firt commit

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+;            
+            Console.ReadLine();
+        }
+    }
+}
