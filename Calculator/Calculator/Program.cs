ea1196a5d2086b56a3236bf0645a41acc4b17ba1
Kate Kalashnikova <kkalashnikova@kruxanalytics.com>
2021-06-14 10:57:36 -0600

    add =,-,/,*

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -8,11 +8,29 @@ namespace Calculator
         {
             Console.Write("Enter first number: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("OPerator: ");
+            string op = Console.ReadLine();
+
             Console.Write("Enter second number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(num1 + num2);
-                        
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
             Console.ReadLine();
         }
     }
